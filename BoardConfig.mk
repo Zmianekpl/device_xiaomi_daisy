402fea5d1986b3e04e7f8aad5faeac7bf51fb68f
wertus33333 <valentin.waldner@windowslive.com>
2018-10-02 14:48:36 +0200

    initial daisy device tree oreo

---
+++ BoardConfig.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from common msm8953-common
+-include device/xiaomi/msm8953-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/daisy
+
+# Filesystem
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_NO_RECOVERY := true
+
+# Kernel
+TARGET_KERNEL_CONFIG := daisy-perf_defconfig
+
+# Partitions
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 55087422464 # 25765059584 - 16384
+
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
+
+# Properties
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+
+# Root Folders
+BOARD_ROOT_EXTRA_FOLDERS := dsp firmware persist
+
+# Sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/daisy/BoardConfigVendor.mk
