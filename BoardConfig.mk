43c6934296c5463f9dc15badf42d3f5f00d54a34
wertus33333 <valentin.waldner@windowslive.com>
2018-10-26 01:56:25 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,32 +23,21 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
 
-# Kernel
-TARGET_KERNEL_CONFIG := daisy-perf_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 veritykeyid=id:5560e7863b4d8118c2f1b065595cf93bb2447992
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x01000000
-TARGET_USES_UEFI := true
-ENABLE_SCHEDBOOST := true
-
-# Partitions
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 55087422464
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x04000000
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
-BOARD_USES_RECOVERY_AS_BOOT := true
-BOARD_HAS_REMOVABLE_STORAGE := true
-
 BOARD_SUPPRESS_SECURE_ERASE := true
 
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_SYSTEMIMAGE_USE_EXT4 := true
+# Kernel
+TARGET_KERNEL_CONFIG := daisy-perf_defconfig
+
+# Partitions
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 52396640000
+# BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x04000000
+
+# Root Folders
+BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
+
+#Storage
+BOARD_HAS_REMOVABLE_STORAGE := true
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
@@ -56,35 +45,30 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 # Properties
 #TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
-# Root Folders
-BOARD_ROOT_EXTRA_FOLDERS := dsp firmware persist
-
-# Vendor
-
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # TWRP Configuration
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_WIPE := /home/oreo/los/device/xiaomi/daisy/recovery/root/etc/recovery.wipe
-AB_OTA_UPDATER := true
-TW_THEME := portrait_hdpi
-TW_INCLUDE_CRYPTO := true
-TW_MAX_BRIGHTNESS := 101
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_EXCLUDE_SUPERSU := true
-TW_EXTRA_LANGUAGES := true
-TW_NO_SCREEN_BLANK := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-RECOVERY_SDCARD_ON_DATA := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
+#BOARD_HAS_LARGE_FILESYSTEM := true
+#BOARD_HAS_NO_SELECT_BUTTON := true
+#TARGET_RECOVERY_WIPE := /home/oreo/los/device/xiaomi/daisy/recovery/root/etc/recovery.wipe
+##AB_OTA_UPDATER := true
+#TW_THEME := portrait_hdpi
+#TW_INCLUDE_CRYPTO := true
+#TW_MAX_BRIGHTNESS := 101
+#TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness
+#TW_EXCLUDE_SUPERSU := true
+#TW_EXTRA_LANGUAGES := true
+#TW_NO_SCREEN_BLANK := true
+#TW_INPUT_BLACKLIST := "hbtp_vm"
+#TARGET_RECOVERY_QCOM_RTC_FIX := true
+#TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+#RECOVERY_SDCARD_ON_DATA := true
+#RECOVERY_GRAPHICS_USE_LINELENGTH := true
+#TW_EXCLUDE_DEFAULT_USB_INIT := true
 
-TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
-TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT}/system/lib64/android.hardware.boot@1.0.so
+#TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
+#TW_RECOVERY_ADDITIONAL_RELINK_FILES := ${OUT}/system/lib64/android.hardware.boot@1.0.so
 
 # Security Patch Hack to prevent Anti Rollback
 PLATFORM_SECURITY_PATCH := 2018-07-01
