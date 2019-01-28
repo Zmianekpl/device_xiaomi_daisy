c859b0eec4612f7e7f206786c7afcb976a37a22d
wertus33333 <valentin.waldner@windowslive.com>
2019-01-28 14:31:03 +0100

    fix vndk

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,43 +1,10 @@
-VNDK_SP_LIBRARIES := \
-    android.hardware.graphics.allocator@2.0 \
-    android.hardware.graphics.common@1.0 \
-    android.hardware.graphics.mapper@2.0 \
-    android.hardware.renderscript@1.0 \
-    android.hidl.memory@1.0 \
-    libRSCpuRef \
-    libRSDriver \
-    libRS_internal \
-    libbacktrace \
-    libbase \
-    libbcinfo \
-    libblas \
-    libc++ \
-    libcompiler_rt \
-    libcutils \
-    libft2 \
-    libhardware \
-    libhidlbase \
-    libhidlmemory \
-    libhidltransport \
-    libhwbinder \
-    libion \
-    liblzma \
-    libpng \
-    libunwind \
-    libutils \
-    libz
-
-EXTRA_VENDOR_LIBRARIES := \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    vendor.display.color@1.0 \
-    vendor.display.config@1.0
-
-
-#-------------------------------------------------------------------------------
-# VNDK Modules
-#-------------------------------------------------------------------------------
+ifeq ($(BOARD_VNDK_VERSION),)
+$(warning ************* BOARD VNDK is not enabled - compiling vndk-sp ***************************)
 LOCAL_PATH := $(call my-dir)
+
+include $(LOCAL_PATH)/vndk-sp-libs.mk
+
+vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
 
 define define-vndk-lib
 include $$(CLEAR_VARS)
@@ -49,6 +16,7 @@ LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := $3
 LOCAL_VENDOR_MODULE := $4
 include $$(BUILD_PREBUILT)
@@ -64,31 +32,27 @@ LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := $3
 LOCAL_VENDOR_MODULE := $4
 include $$(BUILD_PREBUILT)
-endif  # TARGET_TRANSLATE_2ND_ARCH is not true
-endif  # TARGET_2ND_ARCH is not empty
+endif # TARGET_TRANSLATE_2ND_ARCH is not true
+endif # TARGET_2ND_ARCH is not empty
 endef
 
 $(foreach lib,$(VNDK_SP_LIBRARIES),\
-    $(eval $(call define-vndk-lib,$(lib),vndk-sp-gen,vndk-sp,)))
-$(foreach lib,$(VNDK_SP_EXT_LIBRARIES),\
-    $(eval $(call define-vndk-lib,$(lib),vndk-sp-ext-gen,vndk-sp,true)))
+    $(eval $(call define-vndk-lib,$(lib),vndk-sp-gen,$(vndk_sp_dir),)))
 $(foreach lib,$(EXTRA_VENDOR_LIBRARIES),\
     $(eval $(call define-vndk-lib,$(lib),vndk-ext-gen,,true)))
-
-
-#-------------------------------------------------------------------------------
-# Phony Package
-#-------------------------------------------------------------------------------
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := vndk-sp
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := \
     $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES)) \
-    $(addsuffix .vndk-sp-ext-gen,$(VNDK_SP_EXT_LIBRARIES)) \
     $(addsuffix .vndk-ext-gen,$(EXTRA_VENDOR_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
 
+vndk_sp_dir :=
+
+endif
