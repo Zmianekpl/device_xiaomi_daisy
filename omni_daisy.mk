03be794167e6868e59c1b30d2901408c9bfbab38
wertus33333 <valentin.waldner@windowslive.com>
2019-02-11 18:51:32 +0100

    update blobs

--- omni_daisy.mk
+++ omni_daisy.mk
@@ -1,3 +1,6 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -11,36 +14,24 @@
 # limitations under the License.
 #
 
+# Release name
 PRODUCT_RELEASE_NAME := daisy
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
-$(call inherit-product, device/xiaomi/daisy/full_daisy.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-# must be before including omni part
-TARGET_BOOTANIMATION_SIZE := 1080p
-
-# Inherit some common Omni stuff.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_daisy
 PRODUCT_DEVICE := daisy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI A2 Lite
 PRODUCT_MANUFACTURER := Xiaomi
+
 TARGET_VENDOR_PRODUCT_NAME := daisy
 TARGET_VENDOR_DEVICE_NAME := daisy
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="daisy" \
-    PRODUCT_NAME="daisy" \
-    BUILD_FINGERPRINT="xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.3.0.PDLMIXM:user/release-keys"
+    TARGET_DEVICE=daisy \
+    BUILD_PRODUCT=daisy \
+    PRODUCT_NAME=daisy
